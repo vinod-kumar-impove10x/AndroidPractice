0f56610d8aff1cfa4606554857f1652183bf6549
Vinodkumar M <vinod.kumar@improve10x.com>
2023-01-30 15:26:04 +0530

    Implemented Progress Bar functionality.

--- app/src/main/java/com/improve10x/androidpractice/ProgressBarActivity.java
+++ app/src/main/java/com/improve10x/androidpractice/ProgressBarActivity.java
@@ -2,14 +2,93 @@ package com.improve10x.androidpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class ProgressBarActivity extends AppCompatActivity {
+
+    Button downloadFileBtn;
+    ProgressDialog progressBar;
+    private int progressBarStatus = 0;
+    private Handler progressBarHandler = new Handler();
+    private long fileSize = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_bar);
         getSupportActionBar().setTitle("Progress Bar");
+
+        addListenerOnButtonClick();
+    }
+
+    private void addListenerOnButtonClick() {
+        downloadFileBtn = findViewById(R.id.download_file_btn);
+        downloadFileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar = new ProgressDialog(view.getContext());
+                progressBar.setCancelable(true);
+                progressBar.setMessage("File downloading ...");
+                progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressBar.setProgress(0);
+                progressBar.setMax(100);
+                progressBar.show();
+                //reset progress bar and fileSize status
+                progressBarStatus = 0;
+                fileSize = 0;
+
+                new Thread(new Runnable() {
+                    public void run() {
+                        while (progressBarStatus < 100) {
+                            // performing operation
+                            progressBarStatus = doOperation();
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            // Updating the progress bar
+                            progressBarHandler.post(new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(progressBarStatus);
+                                }
+
+
+                            });
+                        }
+                        if (progressBarStatus >= 100) {
+                            // sleeping for 1 second after operation completed
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            // close the progress bar dialog
+                            progressBar.dismiss();
+                        }
+                    }
+                }).start();
+            }//end of onClick method
+        });
+    }
+
+    private int doOperation() {
+        while (fileSize <= 10000) {
+            fileSize++;
+            if (fileSize == 1000) {
+                return 10;
+            } else if (fileSize == 2000) {
+                return 20;
+            } else if (fileSize == 3000) {
+                return 30;
+            } else if (fileSize == 4000) {
+                return 40; // you can add more else if
+            }
+        }
+        return 100;
     }
 }
