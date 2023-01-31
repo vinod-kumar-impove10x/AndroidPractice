e83e0d18f3e375e8341e974a849e1f3b5ce982a3
Vinodkumar M <vinod.kumar@improve10x.com>
2023-01-31 15:56:02 +0530

    Implemented Progress Dialog functionality.

--- app/src/main/java/com/improve10x/androidpractice/ProgressDialogActivity.java
+++ app/src/main/java/com/improve10x/androidpractice/ProgressDialogActivity.java
@@ -2,18 +2,32 @@ package com.improve10x.androidpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.improve10x.androidpractice.databinding.ActivityAlertDialogBinding;
 
 public class ProgressDialogActivity extends AppCompatActivity {
 
-    ActivityAlertDialogBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_dialog);
         getSupportActionBar().setTitle("Progress Dialog");
+
+        showProgressDialog();
+    }
+
+    private void showProgressDialog() {
+
+        Button submitBtn = findViewById(R.id.submit_btn);
+        submitBtn.setOnClickListener(view -> {
+            ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("This is Title");
+            progressDialog.setTitle("This is Message");
+            progressDialog.show();
+        });
     }
 }
